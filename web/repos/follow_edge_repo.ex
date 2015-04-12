93ecab92a6fbcdd0f7af9c4cf2270af6c1680d17
Anton Mishchuk <anton.mishchuk@gmail.com>
2015-04-12 15:43:29 +0300

    prepare

--- web/repos/follow_edge_repo.ex
+++ web/repos/follow_edge_repo.ex
@@ -18,4 +18,10 @@ defmodule Octograph.FollowEdgeRepo do
     Octograph.Repo.all(query)
 	end
 
+	def from_nodes(user_nodes) do
+		to_ids = Enum.map(user_nodes, &(&1.id))
+		query = from f in Octograph.FollowEdge, where: f.from in ^to_ids, select: f
+    Octograph.Repo.all(query)
+	end
+
 end
