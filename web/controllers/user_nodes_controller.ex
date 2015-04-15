fbfb715a0d6da99cba17ca86c8cc4374acfed5f4
Anton Mishchuk <anton.mishchuk@onapp.com>
2015-04-15 17:36:17 +0300

    followers spider

--- web/controllers/user_nodes_controller.ex
+++ web/controllers/user_nodes_controller.ex
@@ -6,7 +6,7 @@ defmodule Octograph.UserNodesController do
   def index(conn, _params) do
     user_node = Octograph.UserNodeRepo.last
     count = Octograph.UserNodeRepo.count
-    edges_count = Octograph.UserNodeRepo.count
+    edges_count = Octograph.FollowEdgeRepo.count
     render conn, "index.html", user_node: user_node, count: count, edges_count: edges_count
   end
 end
